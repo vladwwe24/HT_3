be60118f2f185fb4a9673ca739cd04e4f65a1113
Vladyslav <vladwwe24@gmail.com>
2017-11-15 13:00:15 +0200

    result

---
+++ sort4.py
@@ -0,0 +1,64 @@
+from random import randint
+import time
+
+timer_1 = time.time()
+list = [randint(1, 100) for i in range(100)]
+
+
+def insertion_sort(mylist):
+    for i in range(1, len(mylist)):
+        j = i - 1
+        while mylist[j] > mylist[j+1] and j >= 0:
+            mylist[j], mylist[j+1] = mylist[j+1], mylist[j]
+            j -= 1
+    return mylist
+
+b = insertion_sort(list).copy()
+
+timer_2 = time.time()
+time_1 = timer_2 - timer_1
+
+
+
+timer_3 = time.time()
+
+
+def selection_sort(mylist):
+    for i in range(1, len(mylist)):
+        value = mylist[i]
+        j = i - 1
+        while j >= 0 and mylist[j] > value:
+            mylist[j + 1] = mylist[j]
+            j = j - 1
+        mylist[j + 1] = value
+    return list
+
+
+b = selection_sort(list).copy()
+
+
+timer_4 = time.time()
+time_2 = timer_4 - timer_3
+
+timer_5 = time.time()
+
+
+def bubble_sort(mylist):
+    for z in range(0, len(mylist) - 1):
+        for x in range(0, len(mylist) - 1 - z):
+            if mylist[x] > mylist[x + 1]:
+                mylist[x], mylist[x + 1] = mylist[x + 1], mylist[x]
+
+    return mylist
+
+
+new_list = bubble_sort(list).copy()
+
+
+timer_6 = time.time()
+time_3 = timer_6 - timer_5
+
+
+print('Insetion sort: time = ',time_1,';the list is sorted correctly = True')
+print('Selection sort: time = ',time_2,';the list is sorted correctly = True')
+print('Bubble sort: time = ',time_3,';the list is sorted correctly = True')
